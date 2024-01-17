1e7c12d89cc769166549503761c9408b77f66f34
Art Walker <AWalker@descartes.com>
2024-01-17 07:28:51 -0500

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiApp_StripBitcodeIssue
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
